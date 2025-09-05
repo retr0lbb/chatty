e7752ab087c8179cd7ac19f7af1117c82d7014ea
retr0lbb <retr0lbb@gmail.com>
2025-09-05 17:41:30 -0300

    feat: init

---
+++ controllers/tarefas-controller.js
@@ -0,0 +1,35 @@
+
+const TarefasModel = require('../models/tarefas-model'); 
+ 
+module.exports = { 
+    exibirLista(req, res) { 
+        const tarefas = TarefasModel.listar(); 
+        res.render('listaTarefas', { tarefas }); 
+    }, 
+    exibirAdicionarTarefa(req, res) { 
+        res.render('adicionarTarefa'); 
+    }, 
+    adicionarTarefa(req, res) { 
+        const novaTarefa = { 
+            descricao: req.body.descricao, 
+            concluida: false 
+        }; 
+        TarefasModel.adicionar(novaTarefa); 
+        res.redirect('/'); 
+    }, 
+    exibirEdicao(req, res) { 
+        const { id } = req.params; 
+        const tarefa = TarefasModel.listar().find((t) => t.id == id); 
+        res.render('editaTarefa', { tarefa }); 
+    }, 
+    editarTarefa(req, res) { 
+        const { id } = req.params; 
+        TarefasModel.editar(id, req.body); 
+        res.redirect('/'); 
+    }, 
+    excluirTarefa(req, res) { 
+        const { id } = req.params; 
+        TarefasModel.excluir(id); 
+        res.redirect('/'); 
+    } 
+};
---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express'); 
+const exphbs = require('express-handlebars'); 
+const bodyParser = require('body-parser'); 
+const tarefasController = require('./controllers/tarefas-controller'); 
+ 
+const app = express(); 
+app.engine('handlebars', exphbs.engine({ 
+    defaultLayout: 'layout',
+    layoutsDir: __dirname + '/views/'
+})); 
+app.set('view engine', 'handlebars'); 
+app.use(bodyParser.urlencoded({ extended: true })); 
+app.use(express.static('public')); 
+ 
+// Rotas 
+app.get('/', tarefasController.exibirLista); 
+app.get('/tarefas/adicionar', tarefasController.exibirAdicionarTarefa); 
+app.post('/tarefas', tarefasController.adicionarTarefa); 
+app.get('/tarefas/:id/editar', tarefasController.exibirEdicao); 
+app.post('/tarefas/:id/editar', tarefasController.editarTarefa); 
+app.get('/tarefas/:id/excluir', tarefasController.excluirTarefa); 
+ 
+// Iniciar o servidor 
+app.listen(3000, () => { 
+    console.log("Servidor rodando em http://localhost:3000"); 
+}); 
